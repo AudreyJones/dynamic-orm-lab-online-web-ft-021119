d58ec54e8dd476efbd5e5d01a862a3b4ab4a9aec
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-25 03:46:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,7 +52,6 @@ class InteractiveRecord
   end
 
   def self.find_by(attr)
-    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{attr.keys[0].to_s} = '#{attr.values[0]}'"
     DB[:conn].execute(sql)
   end
