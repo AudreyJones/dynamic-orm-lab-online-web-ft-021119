9d78f715d5a55bd2369a1080468cb313a7c3815f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-25 03:33:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by
+  def self.find_by(attr)
     # sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     # DB[:conn].execute(sql)
   end
