62bdf563f17805048e58342e9797b77d1980d744
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-25 03:34:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(column)
+  def self.find_by(hash)
     binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE attr = '#{attr}'"
     DB[:conn].execute(sql)
